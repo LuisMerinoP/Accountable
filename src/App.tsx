e8c5c234d2c2dd2ed709aea16b738d1ec12f71f7
rustyBucketBay <merino.p.luis@gmail.com>
2021-02-22 23:02:37 +0100

    - code clean on React Router render type fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch } from 'react-router-dom';
+import { Route } from 'react-router';
 import NavBar from './components/layout/navbar';
 import Dashboard from './components/dashboard/dashboard';
 import ProjectDetails from './components/projects/projectDetails';
 import SignIn from './components/auth/signIn';
 import SignUp from './components/auth/signUp';
 import CreateProject from './components/projects/createProject';
-//import firebase from 'firebase/app';
-// import { RouteComponentProps } from 'react-router-dom';
 import { IFirebaseProject } from './store/types/projectTypes';
 import { Location } from 'history';
-//import {match} from 'react-router';
-//import { StaticContext } from 'react-router'
-// import {
-//   BrowserRouter as Router,
-//   Route,
-//   Link,
-//   RouteComponentProps
-// } from "react-router-dom";
-//import { RouteProps } from "react-router";
 
 function App() {
   return (
@@ -28,7 +18,7 @@ function App() {
           <Route exact path='/' component={Dashboard}/>
           <Route
             path='/project/:id'
-            render={({ location }: { location: Location<{ project: IFirebaseProject }> }) => {
+            render={({ location }: {location: Location<{project: IFirebaseProject}>})  => {
                 const { state } = location;
                 return <ProjectDetails project={state.project} />
             }}
--- src/components/projects/projectDetails.tsx
+++ src/components/projects/projectDetails.tsx
@@ -1,70 +1,23 @@
-import { connect, ConnectedProps } from 'react-redux';
-import { firestoreConnect } from 'react-redux-firebase';
-import { compose } from 'redux';
 import { IFirebaseProject } from '../../store/types/projectTypes';
-import { RootState } from '../../store/reducers/rootReducer';
 import { Redirect } from 'react-router-dom';
 import moment from 'moment';
 import firebase from 'firebase/app';
-import { useLocation } from 'react-router-dom';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  RouteComponentProps
-} from "react-router-dom";
-import { createNotEmittedStatement } from 'typescript';
-import { IframeHTMLAttributes } from 'react';
-
-// const mapStateToProps = (state:RootState, ownProps:any) => {
-//   const id = ownProps.match.params.id;
-//   const projects = state.firestore.data.projects;
-//   const project = projects ? projects[id] : null;
-//   return {
-//     project: project,
-//     auth: state.firebase.auth    
-//   }//
-// }
-
-// const connector = connect(mapStateToProps);
-// type Props = ConnectedProps<typeof connector> 
-
-// const ProjectDetails = ({ match }: RouteComponentProps<TParams>) => {
-//type TParams = { id: string | undefined };
-
 
 const ProjectDetails = ({ project }: { project: IFirebaseProject }) => {
-  // const location = useLocation();
-  // console.log(location.pathname)
-  // const project = {
-    
-  //   id: 'cabrón',
-  //   authorFirstName: 'Pablo',
-  //   authorId: 'trialId',
-  //   authorLastName: 'Merino',
-  //   content: 'Mierda',
-  //   createdAt: new firebase.firestore.Timestamp(0,0), //firebase timestamp
-  //   title: 'dummy title'
-    
-  // }
-
-  //const projectId = match.params.id;
-  //console.log(props.state);
-  //const project = props.state;
   if (!firebase.auth().currentUser) return <Redirect to='/'/>
-  if (props.project) {
+  if (project) {
     return(
       <div className="container section project-details">
         <div className="card z-depth-0">
           <div className="card-content">
-            <span className="card-title">{props.project.title}</span>
+            <span className="card-title">{project.title}</span>
             <p>
-              {props.project.content}
+              {project.content}
             </p>
           </div>
           <div className="card-action grey lighten-4">
-            <div>{props.project.authorFirstName} {props.project.authorLastName}</div>
-            <div>{moment(props.project.createdAt.toDate()).calendar()}</div>
+            <div>{project.authorFirstName} {project.authorLastName}</div>
+            <div>{moment(project.createdAt.toDate()).calendar()}</div>
           </div>
         </div>
       </div>
@@ -78,10 +31,4 @@ const ProjectDetails = ({ project }: { project: IFirebaseProject }) => {
   }
 }
 
-// export default compose<React.FunctionComponent>(
-//   connector,
-//   firestoreConnect([
-//     { collection: 'projects' }
-//   ])
-// )(ProjectDetails);
 export default ProjectDetails;
