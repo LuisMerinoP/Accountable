77c0901e6cc5edfe60fe50535ad09e7bf3506ff5
rustyBucketBay <merino.p.luis@gmail.com>
2021-05-03 22:36:57 +0200

    - changed materialize.js code for 12h to be 0h. Changes after in the frequency set do not need to be done anymore

--- src/components/projects/frequencySet.tsx
+++ src/components/projects/frequencySet.tsx
@@ -17,35 +17,16 @@ const FrequencySet =  ( props : { clearLableCallback:() => void }) => {
       defaultTime: "00:00",
       showClearBtn: true,
       onOpenStart: () => { 
-        // set 12 to 00 in picker
-        let timePickerDivs = document.getElementsByClassName('timepicker-tick');
-        let twelveDiv = Array.from(timePickerDivs).find(element => element.innerHTML === '12')
-        if (twelveDiv) twelveDiv.innerHTML = '00';
-        // remove AM PM labels from display
         const ampmLabels = document.querySelector('.timepicker-span-am-pm');
         ampmLabels?.classList.add('hidden');
-        if (instance.hours === 12 || !instance.hours || instance.time === '') {
-          // set 12 to 00 in digital display
-          let hourDigitalDisplay = document.querySelector('.timepicker-span-hours.text-primary');
-          if (hourDigitalDisplay) hourDigitalDisplay.innerHTML = '00';
-        }
-      },
-      onSelect:(hour:number, minute:number) => {
-        if (hour === 12) {
-          // set 12 to 00 in digital display
-          let hourDigitalDisplay = document.querySelector('.timepicker-span-hours');
-          if (hourDigitalDisplay) hourDigitalDisplay.innerHTML = '00';
-        }
       },
       onCloseStart:() => {
-        let value = myInputRef.current?.value.replace('AM','').replace('PM','').replace('12','00').replace(/\s/g, "");
+        let value = myInputRef.current?.value.replace('AM','').replace('PM','').replace(/\s/g, "");
         if (value && myInputRef.current) myInputRef.current.value = value;
         props.clearLableCallback();
       }
     }
     if (elem) M.Timepicker.init(elem, options);
-    let instance = {} as ExtendedTimePicker;
-    if (elem) { instance = M.Timepicker.getInstance(elem) as ExtendedTimePicker; }
   },[props]);
 
   return (
